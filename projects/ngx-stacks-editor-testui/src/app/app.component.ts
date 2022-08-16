0309cea11207671253bbaba126f1ee94be8c3dff
Adam Plocher <adam@bitcollectors.com>
2022-08-16 11:27:40 -0700

    Updated example

--- projects/ngx-stacks-editor-testui/src/app/app.component.ts
+++ projects/ngx-stacks-editor-testui/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
   title = 'ngx-stacks-editor-testui';
+  markdown = `# My book title
+
+  ## Chapter 1: Who done it?
+
+  This is a test
+
+  * Item 1
+  * Item 2
+    - Item 2 (a)
+    - Item 2 (b)
+
+  1. Item 1
+  2. Item 2
+  `;
+
+  ngOnInit(): void {
+    
+  }
 }
